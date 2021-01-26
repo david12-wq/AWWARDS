7d9cbbfd895eabe2fb445fec657edce54f4640c9
david12-wq <davidkamau245@gmail.com>
2021-01-26 20:40:31 +0300

    add form.py

---
+++ Awwards/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+class UpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'biography', 'image']   
+
+class ProjectForm(forms.ModelForm):
+
+    class Meta:
+        model= Projects
+        exclude= ['author', 'created_date', 'author_profile'] 
